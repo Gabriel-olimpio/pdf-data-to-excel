70e1215c4d1e7ff5eb2d64da7a13697da3f440dd
Gabriel <gabrielandre699@gmail.com>
2024-05-09 13:57:37 -0300

    new comments

--- project.py
+++ project.py
@@ -14,20 +14,22 @@ def get_file(directory):
     files = os.listdir(directory)
     for f in files:
         try:
-            if 'fatura_' in f:
+            if 'Nubank_' in f:  # Search file's name
                 return f
         except FileNotFoundError:
             print(FileNotFoundError)
 
 
+# The data is located at the end of pdf (page 4 -> index 3)
 def open_pdf(file):
     with pdfplumber.open(file) as pdf:
-        page = pdf.pages[3]
+        page = pdf.pages[3]  # data location
         table = page.extract_table()
         organize_list(table)
         return table  # just for pytest
 
 
+# removing empty strings on list
 def organize_list(data):
     for i in range(len(data)):
         data[i].pop(0)
@@ -39,18 +41,17 @@ def py_excel(d):
     book = load_workbook('Planilha modif nova.xlsx')  # worksheet wanted
 
     # creating page
+    # book.create_sheet('Nubank Fatura')  # create sheet page or
+    fatura_page = book['Nubank Fatura']  # access sheet page
 
-    # book.create_sheet('Nubank Fatura')
-    fatura_page = book['Nubank Fatura']
-
+    # insert values in each cell
     for i, row in enumerate(d):
         for j, value in enumerate(row):
             fatura_page.cell(row=i + 2, column=j + 1, value=value)
 
-    # printing values
+    # printing values in console
     for row in fatura_page.iter_rows(min_row=2, max_row=len(d), max_col=2, values_only=True):
         print(row)
-
     for row in fatura_page.iter_rows(min_row=2, max_row=len(d), max_col=2, values_only=False):
         print(row)
 
@@ -61,6 +62,7 @@ def main():
     d = get_directory()
     user = get_file(d)
     open_pdf(d+user)
+
 
 if __name__ == "__main__":
     main()
